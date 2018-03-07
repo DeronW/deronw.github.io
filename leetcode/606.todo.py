77c6df382328a3744e136736e0f44fa8e6b00fe2
DeronW <w.del@qq.com>
2018-03-07 12:35:11 +0800

    I'm too lazy, nothing left

---
+++ leetcode/606.todo.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tree2str(self, t):
+        """
+        :type t: TreeNode
+        :rtype: str
+        """
+        
+        r = ''
+
+        while t:
+            r += t.va
+
+        return r
