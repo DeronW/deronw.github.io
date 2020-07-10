84696a58e3d790a6f6a8b7e738b7864f873e6864
delong <w.del@qq.com>
2020-07-10 19:56:42 +0800

    Revert "add a Demo, revert this commit 1 week later"
    This reverts commit 1269ef150093435cb9d4b0089dcad27018b315e7.

--- web_modules/react-dom.js
+++
@@ -1,3 +0,0 @@
-import './common/index-8467b4e0.js';
-import { r as reactDom } from './common/index-f0ae2d7f.js';
-export { r as default } from './common/index-f0ae2d7f.js';
--- web_modules/react.js
+++
@@ -1,8 +0,0 @@
-import { r as react } from './common/index-8467b4e0.js';
-export { r as default } from './common/index-8467b4e0.js';
-
-
-
-var useEffect = react.useEffect;
-var useState = react.useState;
-export { useEffect, useState };
